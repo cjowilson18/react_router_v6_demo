94e9e5cf7ec05ed8448c5a69b57f7f79d578993e
CJ <Cjowilson18@gmail.com>
2023-10-18 13:08:10 -0400

    Basics of V6_7.8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { BrowserRouter, Routes, Route, Link, Outlet, useParams, } from 'react-router-dom';
+
+const NavBar = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h2>React Rotuer v6 Demo</h2>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="press">Press</Link>
+        </li>
+      </ul>
+    </nav>
+    <Outlet />
+  </>
+  )
+}
+
+const Home = function () {
+  return (
+    <h1> Honey I'm Home </h1>
+  )
+}
+
+const Article = function () {
+  let {article_id} = useParams();
+  return <h3>Article ID: {article_id}</h3>;
+}
+const Archive = function () {
+  let {year} = useParams();
+  return <h3>Year {year}</h3>;
+}
+const Press = function () {
+  return (
+    <>
+      <h1>Press</h1>
+      <div><Link to="1">Why is the sky blue?</Link></div>
+      <div><Link to="2">The science of solar eclipse?</Link></div>
+      <nav>
+        <p>Archive</p>
+        <ul>
+          <li>
+            <Link to="archive/2023">2023</Link>
+          </li>
+          <li>
+            <Link to="archive/2022">2022</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path=":article_id" element={<Article />} />
+        <Route path="archive/:year" element={<Archive />} />
+      </Routes>
+    </>
+  )
+}
+const App = function () {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="press" element={<Press />} />
+        <Route path=":article_id" element={<Article />} />
+        <Route path="archive/:year" element={<Archive />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
